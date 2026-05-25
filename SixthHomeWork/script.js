d64602f985109976e781e2c06747d827a038f6f9
Alex Todua <alx.todua777@gmail.com>
2026-05-25 23:33:47 +0400

    Adding files

---
+++ SixthHomeWork/script.js
@@ -0,0 +1,101 @@
+// ამოცანა 1 — ლუწი თუ კენტი
+
+// const n=parseInt(prompt("Input the number"))
+
+// function isEven(n){
+//     if(n%2===0){
+//         document.write(`(${n}) figure is even`);
+//     }else if(n%2!==0){
+//         document.write(`(${n}) figure is odd`);
+//     }
+// }
+
+// isEven(n);
+
+// --------------------------------------------------
+
+// ამოცანა 2 — სამი რიცხვის მაქსიმუმი
+// let a=parseInt(prompt("Input the first number"));
+// let b=parseInt(prompt("Input the second number"));
+// let c=parseInt(prompt("Input the third number"));
+// let arr=[a,b,c];
+// document.write(`Your numbers: (${arr})`);
+// function getMax(a,b,c){
+//     if(a>b && a>c){
+//         document.write(`The (${a}) is greater than (${b}) and (${c})`);
+//     }else if(b>a && b>c){
+//         document.write(`The (${b}) is greater than (${a}) and (${c})`);
+//     }else if(c>a && c>b){
+//         document.write(`The (${c}) is greater than (${a}) and (${b})`);
+//     }
+// }
+
+// getMax(a,b,c);4
+
+// -----------------------------------------------------
+
+// ამოცანა 3 — ქულიდან შეფასება
+
+// let score=Math.round(Math.random()*100);
+// document.write(score);
+
+// function getGrade(score){
+//     switch (score) {
+//         case score:
+//             if(score>=90 && score<=100){
+//                 document.write("A-Grade");
+//             }else if(score>=80 && score <=89){
+//                 document.write("B-Grade");
+//             }else if(score>=70 && score<=79){
+//                 document.write("C-Grade");
+//             }else if(score>=60 && score<=69){
+//                 document.write("D-grade");
+//             }else{
+//                 document.write("F-Grade");
+//             }
+            
+//             break;
+    
+//         default:
+//             break;
+//     }
+// }
+
+// getGrade(score);
+
+// --------------------------------------------------------------
+
+// ამოცანა 4 — მასივის ჯამი
+
+// function sumArray(arr){
+//     let sum=0;
+//     for(let i=0;i<arr.length;i++){
+//         sum+=arr[i];
+//     }
+//     return sum;
+// }
+
+// console.log(sumArray([3,4,5]));
+
+// --------------------------------------------------------------
+
+// ამოცანა 5 — ტემპერატურის კონვერტაცია
+
+// alert("Tempeture converter C to F or F to C");
+
+// let value =+prompt("Input the value");
+// let unit =prompt("Choose tempeture unit").toUpperCase();
+
+// function convertTemp(value,unit){
+//     let result=0;
+//     if(unit=="C"){
+//         result+=Math.round((value*9/5)+32);
+//     }else if(unit=="F"){
+//         result+=Math.round((value-32)*5/9);
+//     }else{
+//         alert("Uknow unit");
+//     }
+//     return result;
+// }
+
+// document.write(convertTemp(value,unit));
